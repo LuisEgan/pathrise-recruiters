87aa84adad160348e1ae9882086c6eba99917789
Luis Egan <luisdegan10@hotmail.com>
2024-04-12 16:09:17 -0600

    loading height

--- src/app/[company]/loading.tsx
+++ src/app/[company]/loading.tsx
@@ -2,7 +2,7 @@ import LoadingAnim from "@/components/Loading/LoadingAnim";
 
 const Loading = () => {
   return (
-    <main className="w-full h-[100vh] flex justify-center pt-10">
+    <main className="w-full h-[70vh] flex justify-center pt-10">
       <LoadingAnim />
     </main>
   );
