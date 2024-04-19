afdb288566e25691b81e45be58c6bf90858582da
Luis Egan <luisdegan10@hotmail.com>
2024-04-19 17:12:03 -0600

    TEST - remove layout footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
         <body>
           {children}
           <footer>
-            <Footer />
+            {/* <Footer /> */}
           </footer>
         </body>
       </html>
